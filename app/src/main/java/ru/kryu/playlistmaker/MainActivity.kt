0780ef02f718dba162813738ba3b4f5b50b2b736
DimaK21 <civ02@mail.ru>
2023-04-04 00:29:22 +0300

    ClickListeners

--- app/src/main/java/ru/kryu/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/kryu/playlistmaker/MainActivity.kt
@@ -2,10 +2,34 @@ package ru.kryu.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+
+        val buttonSearchClickListener: View.OnClickListener = object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Поиск", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Медиатека", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }

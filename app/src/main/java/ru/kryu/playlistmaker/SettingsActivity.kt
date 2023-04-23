61a92de045460232c9b7aabb12d146d85c34dbd6
DimaK21 <civ02@mail.ru>
2023-04-23 12:03:03 +0300

    buttonShare

--- app/src/main/java/ru/kryu/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/kryu/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package ru.kryu.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,8 +14,25 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val buttonBack = findViewById<ImageView>(R.id.settings_arrow)
+        val buttonShare = findViewById<FrameLayout>(R.id.share)
+        val buttonSupport = findViewById<FrameLayout>(R.id.support)
+        val buttonAgreement = findViewById<FrameLayout>(R.id.agreement)
+        val email = getString(R.string.email_of_developer)
+        val titleMail = getString(R.string.title_mail_to_developer)
+        val urlPracticum = getString(R.string.url_practicum)
+        val urlUserAgreement = getString(R.string.url_user_agreement)
+
+
         buttonBack.setOnClickListener {
             finish()
+        }
+
+        buttonShare.setOnClickListener{
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, email)
+            shareIntent.putExtra(Intent.EXTRA_TITLE, titleMail)
+            startActivity(shareIntent)
         }
     }
 }

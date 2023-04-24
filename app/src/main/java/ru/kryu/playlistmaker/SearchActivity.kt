3b7224ea88af2e651765d7ffad8b89cef0cb1e0c
DimaK21 <civ02@mail.ru>
2023-04-24 21:08:35 +0500

    TextWatcher

--- app/src/main/java/ru/kryu/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/kryu/playlistmaker/SearchActivity.kt
@@ -1,11 +1,47 @@
 package ru.kryu.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val editText = findViewById<EditText>(R.id.edit_text_search)
+        val clearButton = findViewById<ImageView>(R.id.edit_text_clear)
+        clearButton.setOnClickListener {
+            editText.setText("")
+        }
+
+        val editTextTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+
+        editText.addTextChangedListener(editTextTextWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }

948a610bd217641c635bec3ab4dc9e2fe67aad80
Dmitry Kryushnikov <civ02@mail.ru>
2023-05-21 14:27:43 +0500

    после поворота экрана, треки остаются

--- app/src/main/java/ru/kryu/playlistmaker/Track.kt
+++ app/src/main/java/ru/kryu/playlistmaker/Track.kt
@@ -1,8 +1,12 @@
 package ru.kryu.playlistmaker
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
     val trackTimeMillis: Long, // Продолжительность трека
     val artworkUrl100: String // Ссылка на изображение обложки
-)
+): Parcelable

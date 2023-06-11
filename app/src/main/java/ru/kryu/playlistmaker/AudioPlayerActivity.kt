3af5ef87ff9d8fd9c0d64b79b87cc01c5e53b784
Dmitry Kryushnikov <civ02@mail.ru>
2023-06-12 01:56:35 +0500

    activity_audio_player.xml

---
+++ app/src/main/java/ru/kryu/playlistmaker/AudioPlayerActivity.kt
@@ -0,0 +1,11 @@
+package ru.kryu.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_player)
+    }
+}

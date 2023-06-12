20e7db0be70f5ce1a4726a5bcd88bb056b8d425f
Dmitry Kryushnikov <civ02@mail.ru>
2023-06-12 15:43:22 +0500

    activity_audio_player.xml - закончил Track.kt - добавил свойства

--- app/src/main/java/ru/kryu/playlistmaker/Track.kt
+++ app/src/main/java/ru/kryu/playlistmaker/Track.kt
@@ -5,9 +5,13 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Track(
-    val trackId: Long,
-    val trackName: String, // Название композиции
-    val artistName: String, // Имя исполнителя
-    val trackTimeMillis: Long, // Продолжительность трека
-    val artworkUrl100: String // Ссылка на изображение обложки
+    val trackId: Long = 0, //id трека
+    val trackName: String = "", // Название композиции
+    val artistName: String = "", // Имя исполнителя
+    val trackTimeMillis: Long = 0, // Продолжительность трека
+    val artworkUrl100: String = "", // Ссылка на изображение обложки
+    val collectionName: String = "", // Название альбома
+    val releaseDate: String = "", // Год релиза трека
+    val primaryGenreName: String = "", // Жанр трека
+    val country: String = "", // Страна исполнителя
 ): Parcelable
